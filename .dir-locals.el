6950ce167c6e4cb70969f89b42a66fcfb7b90efc
David A. Ventimiglia <dventimi@gmail.com>
2012-05-23 19:08:50 -0700

    First transferrence of assets from atomic to radium salt

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((java-mode
+  (compile-command . (substitute-env-vars "cd $RADIUMSALT_HOME; make"))))
+
+
