9526d94695426b3cfd033aae8e4e20a780031179
David A. Ventimiglia <dventimi@gmail.com>
2012-05-23 20:52:08 -0700

    Got the very crudest ANTLR parser running

--- src/RadiumSalt.java
+++ src/RadiumSalt.java
@@ -1,3 +1,11 @@
+import org.antlr.runtime.*;
+import org.antlr.runtime.ANTLRInputStream;
+
 public class RadiumSalt {
-    public static void main (String[] args) {
-	System.out.println("Hello, World!");}}
+    public static void main (String[] args) throws Exception {
+	ANTLRInputStream input = new ANTLRInputStream(System.in);
+	TLexer lexer = new TLexer(input);
+	CommonTokenStream tokens = new CommonTokenStream(lexer);
+	TParser parser = new TParser(tokens);
+	parser.r();}}
+
