32837b76111986d7c617fcc2fcbaa42038538b65
David A. Ventimiglia <dventimi@gmail.com>
2018-04-04 16:24:46 -0700

    Got full uri to parse

--- src/main/java/org/protonchamber/ProtonChamber.java
+++ src/main/java/org/protonchamber/ProtonChamber.java
@@ -18,16 +18,7 @@ public class ProtonChamber {
 	parser.setInputString(s);
 	Ast ast = parser.enableAst(true);
 	for (Test.RuleNames r : Test.RuleNames.values()) ast.enableRuleNode(r.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.DUMMYSTARTRULE.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.ODATAURI.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.SERVICEROOT.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.HOST.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.REG_NAME.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.PORT.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.SEGMENT_NZ.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.ODATARELATIVEURI.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.RESOURCEPATH.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.ENTITYSETNAME.ruleID(), true);
-	// ast.enableRuleNode(Test.RuleNames.ODATAIDENTIFIER.ruleID(), true);
+	ast.enableRuleNode(Test.RuleNames.DUMMYSTARTRULE.ruleID(), true);
+	ast.enableRuleNode(Test.RuleNames.
 	Result result = parser.parse();
 	ast.display(out, true);}}
