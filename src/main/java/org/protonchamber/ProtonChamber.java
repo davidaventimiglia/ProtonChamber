48925c7038af3a7fe61aab3f51e7de9e46e494b9
David A. Ventimiglia <dventimi@gmail.com>
2018-06-26 14:35:06 -0700

    Generating parse trees

--- src/main/java/org/protonchamber/ProtonChamber.java
+++ src/main/java/org/protonchamber/ProtonChamber.java
@@ -2,7 +2,7 @@ package org.protonchamber;
 
 import apg.*;
 import apg.Parser.*;
-import apg.Parser.Result;
+// import apg.Parser.Result;
 import java.io.*;
 import java.util.*;
 
@@ -18,6 +18,5 @@ public class ProtonChamber {
 	parser.setInputString(s);
 	Ast ast = parser.enableAst(true);
 	for (Test.RuleNames r : Test.RuleNames.values()) ast.enableRuleNode(r.ruleID(), true);
-	ast.enableRuleNode(Test.RuleNames.DUMMYSTARTRULE.ruleID(), true);
 	Result result = parser.parse();
 	ast.display(out, true);}}
