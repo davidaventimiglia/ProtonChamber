c6d0412eb0112ded7dd160dc41b0d64ed39f0043
David A. Ventimiglia <dventimi@gmail.com>
2018-06-27 19:51:49 -0700

    Got the schemas to come out

--- src/main/java/org/protonchamber/DatabaseMetaDataEdmProvider.java
+++ src/main/java/org/protonchamber/DatabaseMetaDataEdmProvider.java
@@ -16,14 +16,27 @@ public class DatabaseMetaDataEdmProvider extends CsdlAbstractEdmProvider {
 	super();
 	this.m = m;}
 
+    // @Override
+    // public List<CsdlSchema> getSchemas () throws ODataException {
+    // 	try (ResultSet r = m.getSchemas()) {
+    // 	    List<CsdlSchema> schemas = new ArrayList<>();
+    // 	    while (r.next()) {
+    // 		CsdlSchema schema = new CsdlSchema();
+    // 		schema.setNamespace(r.getString("TABLE_SCHEM"));
+    // 		schemas.add(schema);}
+    // 	    return schemas;}
+    // 	catch (Throwable e) {e.printStackTrace(System.out); throw new ODataException(e);}}
+
     @Override
     public List<CsdlSchema> getSchemas () throws ODataException {
-	try (ResultSet r = m.getSchemas()) {
-	    List<CsdlSchema> schemas = new ArrayList<>();
+	try (ResultSet r = m.getColumns(null, null, null, null)) {
+	    Map<FullQualifiedName, CsdlSchema> schemas = new HashMap<>();
+	    Map<FullQualifiedName, CsdlEntityType> entityTypes = new HashMap<>();
 	    while (r.next()) {
-		CsdlSchema schema = new CsdlSchema();
-		schema.setNamespace(r.getString("TABLE_SCHEM"));
-		schemas.add(schema);}
-	    return schemas;}
+		schemas.putIfAbsent(new FullQualifiedName(r.getString("TABLE_SCHEM"), null), new CsdlSchema());
+		entityTypes.putIfAbsent(new FullQualifiedName(r.getString("TABLE_SCHEM"), r.getString("TABLE_NAME")), new CsdlEntityType());
+		schemas.get(new FullQualifiedName(r.getString("TABLE_SCHEM"), null)).setNamespace(r.getString("TABLE_SCHEM"));
+		entityTypes.get(new FullQualifiedName(r.getString("TABLE_SCHEM"), r.getString("TABLE_NAME"))).setName(r.getString("TABLE_NAME"));}
+	    return new ArrayList<>(schemas.values());}
 	catch (Throwable e) {e.printStackTrace(System.out); throw new ODataException(e);}}
 }
