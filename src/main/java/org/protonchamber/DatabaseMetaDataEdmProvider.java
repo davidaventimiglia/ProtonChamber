6e1c2f0958ccee8dc7a13cf85571cb3f2a9f2474
David A. Ventimiglia <dventimi@gmail.com>
2018-06-28 15:26:52 -0700

    I don't know

--- src/main/java/org/protonchamber/DatabaseMetaDataEdmProvider.java
+++ src/main/java/org/protonchamber/DatabaseMetaDataEdmProvider.java
@@ -12,31 +12,80 @@ import org.apache.olingo.commons.api.ex.*;
 public class DatabaseMetaDataEdmProvider extends CsdlAbstractEdmProvider {
     protected DatabaseMetaData m;
 
+    interface Processor {
+	void process (ResultSet r) throws SQLException;}
+
+    static class ProtonRoot implements Processor {
+	Map<String, ProtonSchema> schemas = new HashMap<>();
+	public ProtonRoot () {}
+	@Override
+	public void process (ResultSet r) throws SQLException {
+	    schemas.putIfAbsent(r.getString("TABLE_SCHEM"), new ProtonSchema(r));
+	    for (Processor p : schemas.values()) p.process(r);}
+	public List<CsdlSchema> getSchemas () {
+	    return new ArrayList<>(schemas.values());}}
+
+    static class ProtonSchema extends CsdlSchema implements Processor {
+	Map<String, ProtonEntityType> entityTypes = new HashMap<>();
+	ProtonEntityContainer entityContainer = new ProtonEntityContainer();
+	public ProtonSchema (ResultSet r) throws SQLException {
+	    super();
+	    setNamespace(r.getString("TABLE_SCHEM"));}
+	@Override
+	public void process (ResultSet r) throws SQLException {
+	    entityTypes.putIfAbsent(r.getString("TABLE_NAME"), new ProtonEntityType(r));
+	    for (Processor p : entityTypes.values()) p.process(r);
+	    entityContainer.process(r);}
+	@Override
+	public List<CsdlEntityType> getEntityTypes () {
+	    return new ArrayList<>(entityTypes.values());}
+	@Override
+	public CsdlEntityContainer getEntityContainer () {
+	    return entityContainer;}}
+
+    static class ProtonEntityType extends CsdlEntityType implements Processor {
+	public ProtonEntityType (ResultSet r) throws SQLException {
+	    super();
+	    setName(r.getString("TABLE_NAME"));}
+	@Override
+	public void process (ResultSet r) {}}
+
+    static class ProtonEntityContainer extends CsdlEntityContainer implements Processor {
+	Map<String, ProtonEntitySet> entitySets = new HashMap<>();
+	public ProtonEntityContainer (ResultSet r) throws SQLException {
+	    super();
+	    setName("EntityContainer");}
+	@Override
+	public void process (ResultSet r) {
+	    entitySets.putIfAbsent(r.getString("TABLE_NAME"), new ProtonEntitySet(r));
+	    for (Processor p : entitySets.values()) p.process(r);}
+	@Override
+	public List<CsdlEntitySet> getEntitySets () {
+	    return new ArrayList<>(entitySets.values());}}
+
+    static class ProtonEntitySet extends CsdlEntitySet implements Processor {
+	public ProtonEntitySet (ResultSet r) throws SQLException {
+	    super();
+	    setName(r.getString("TABLE_NAME"));}
+	@Override
+	public void process (ResultSet r) {}}
+
+    static class ProtonEntitySet extends CsdlEntitySet implements Processor {
+	public ProtonEntitySet (ResultSet r) throws SQLException {
+	    super();
+	    setName(r.getString("TABLE_NAME"));}
+	@Override
+	public void process (ResultSet r) {}}
+    
     public DatabaseMetaDataEdmProvider (DatabaseMetaData m) {
 	super();
 	this.m = m;}
 
-    // @Override
-    // public List<CsdlSchema> getSchemas () throws ODataException {
-    // 	try (ResultSet r = m.getSchemas()) {
-    // 	    List<CsdlSchema> schemas = new ArrayList<>();
-    // 	    while (r.next()) {
-    // 		CsdlSchema schema = new CsdlSchema();
-    // 		schema.setNamespace(r.getString("TABLE_SCHEM"));
-    // 		schemas.add(schema);}
-    // 	    return schemas;}
-    // 	catch (Throwable e) {e.printStackTrace(System.out); throw new ODataException(e);}}
-
     @Override
     public List<CsdlSchema> getSchemas () throws ODataException {
+	ProtonRoot root = new ProtonRoot();
 	try (ResultSet r = m.getColumns(null, null, null, null)) {
-	    Map<FullQualifiedName, CsdlSchema> schemas = new HashMap<>();
-	    Map<FullQualifiedName, CsdlEntityType> entityTypes = new HashMap<>();
-	    while (r.next()) {
-		schemas.putIfAbsent(new FullQualifiedName(r.getString("TABLE_SCHEM"), null), new CsdlSchema());
-		entityTypes.putIfAbsent(new FullQualifiedName(r.getString("TABLE_SCHEM"), r.getString("TABLE_NAME")), new CsdlEntityType());
-		schemas.get(new FullQualifiedName(r.getString("TABLE_SCHEM"), null)).setNamespace(r.getString("TABLE_SCHEM"));
-		entityTypes.get(new FullQualifiedName(r.getString("TABLE_SCHEM"), r.getString("TABLE_NAME"))).setName(r.getString("TABLE_NAME"));}
-	    return new ArrayList<>(schemas.values());}
+	    while (r.next()) root.process(r);
+	    return root.getSchemas();}
 	catch (Throwable e) {e.printStackTrace(System.out); throw new ODataException(e);}}
 }
