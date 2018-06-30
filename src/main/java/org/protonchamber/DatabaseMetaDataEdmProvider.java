b21e35db660fb2f11e6d39606d6b92f04037f103
David A. Ventimiglia <dventimi@gmail.com>
2018-06-30 13:41:35 -0700

    Trying to inject properties

--- src/main/java/org/protonchamber/DatabaseMetaDataEdmProvider.java
+++ src/main/java/org/protonchamber/DatabaseMetaDataEdmProvider.java
@@ -20,20 +20,22 @@ public class DatabaseMetaDataEdmProvider extends CsdlAbstractEdmProvider {
 	public ProtonRoot () {}
 	@Override
 	public void process (ResultSet r) throws SQLException {
-	    schemas.putIfAbsent(r.getString("TABLE_SCHEM"), new ProtonSchema(r));
+	    schemas.putIfAbsent(r.getString("TABLE_SCHEM"), new ProtonSchema(this, r));
 	    for (Processor p : schemas.values()) p.process(r);}
 	public List<CsdlSchema> getSchemas () {
 	    return new ArrayList<>(schemas.values());}}
 
     static class ProtonSchema extends CsdlSchema implements Processor {
+	ProtonRoot root;
 	Map<String, ProtonEntityType> entityTypes = new HashMap<>();
 	Map<String, ProtonEntityContainer> entityContainers = new HashMap<>();
-	public ProtonSchema (ResultSet r) throws SQLException {
+	public ProtonSchema (ProtonRoot root, ResultSet r) throws SQLException {
 	    super();
+	    this.root = root;
 	    setNamespace(r.getString("TABLE_SCHEM"));}
 	@Override
 	public void process (ResultSet r) throws SQLException {
-	    if (r.getString("TABLE_SCHEM").equals(getNamespace())) entityTypes.putIfAbsent(r.getString("TABLE_NAME"), new ProtonEntityType(r));
+	    if (r.getString("TABLE_SCHEM").equals(getNamespace())) entityTypes.putIfAbsent(r.getString("TABLE_NAME"), new ProtonEntityType(this, r));
 	    if (r.getString("TABLE_SCHEM").equals(getNamespace())) entityContainers.putIfAbsent("entityContainer", new ProtonEntityContainer(this, r));
 	    for (Processor p : entityTypes.values()) p.process(r);
 	    for (Processor p : entityContainers.values()) p.process(r);}
@@ -46,9 +48,28 @@ public class DatabaseMetaDataEdmProvider extends CsdlAbstractEdmProvider {
 	    throw new IllegalStateException();}}
 
     static class ProtonEntityType extends CsdlEntityType implements Processor {
-	public ProtonEntityType (ResultSet r) throws SQLException {
+	CsdlSchema schema;
+	Map<String, ProtonProperty> properties = new HashMap<>();
+	public ProtonEntityType (CsdlSchema schema, ResultSet r) throws SQLException {
 	    super();
+	    this.schema = schema;
 	    setName(r.getString("TABLE_NAME"));}
+	public CsdlSchema getSchema () {
+	    return schema;}
+	@Override
+	public void process (ResultSet r) throws SQLException {
+	    if (r.getString("TABLE_SCHEM").equals(getSchema().getNamespace())) if (r.getString("TABLE_NAME").equals(getName())) properties.putIfAbsent(r.getString("COLUMN_NAME"), new ProtonProperty(this, r));
+	    for (Processor p : properties.values()) p.process(r);}
+	@Override
+	public List<CsdlProperty> getProperties () {
+	    return new ArrayList<>(properties.values());}}
+
+    static class ProtonProperty extends CsdlProperty implements Processor {
+	CsdlEntityType entityType;
+	public ProtonProperty (CsdlEntityType entityType, ResultSet r) throws SQLException {
+	    super();
+	    this.entityType = entityType;
+	    setName(r.getString("COLUMN_NAME"));}
 	@Override
 	public void process (ResultSet r) {}}
 
@@ -63,15 +84,17 @@ public class DatabaseMetaDataEdmProvider extends CsdlAbstractEdmProvider {
 	    return schema;}
 	@Override
 	public void process (ResultSet r) throws SQLException {
-	    if (r.getString("TABLE_SCHEM").equals(getSchema().getNamespace())) entitySets.putIfAbsent(r.getString("TABLE_NAME"), new ProtonEntitySet(r));
+	    if (r.getString("TABLE_SCHEM").equals(getSchema().getNamespace())) entitySets.putIfAbsent(r.getString("TABLE_NAME"), new ProtonEntitySet(this, r));
 	    for (Processor p : entitySets.values()) p.process(r);}
 	@Override
 	public List<CsdlEntitySet> getEntitySets () {
 	    return new ArrayList<>(entitySets.values());}}
 
     static class ProtonEntitySet extends CsdlEntitySet implements Processor {
-	public ProtonEntitySet (ResultSet r) throws SQLException {
+	CsdlEntityContainer entityContainer;
+	public ProtonEntitySet (CsdlEntityContainer entityContainer, ResultSet r) throws SQLException {
 	    super();
+	    this.entityContainer = entityContainer;
 	    setName(r.getString("TABLE_NAME"));
 	    setType(new FullQualifiedName(r.getString("TABLE_SCHEM"), r.getString("TABLE_NAME")));}
 	@Override
