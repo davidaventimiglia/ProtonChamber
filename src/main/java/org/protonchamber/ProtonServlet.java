26549dd8c35039ba434c4cece8a6f2e6500581e8
David A. Ventimiglia <dventimi@gmail.com>
2018-07-22 07:55:31 -0700

    Minor change

--- src/main/java/org/protonchamber/ProtonServlet.java
+++ src/main/java/org/protonchamber/ProtonServlet.java
@@ -12,12 +12,11 @@ import org.apache.olingo.server.api.*;
 import org.apache.olingo.server.api.debug.*;
 
 public class ProtonServlet extends HttpServlet {
-
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void service (final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-	try (Connection conn = ((DataSource)((Context)(new InitialContext()).lookup("java:comp/env")).lookup("jdbc/ProtonDB2")).getConnection()) {
+	try (Connection conn = ((DataSource)((Context)(new InitialContext()).lookup("java:comp/env")).lookup("jdbc/ProtonDB")).getConnection()) {
 	    OData odata = OData.newInstance();
 	    ServiceMetadata edm = odata.createServiceMetadata(new DatabaseMetaDataEdmProvider(this, conn.getMetaData()), new ArrayList<EdmxReference>());
 	    ODataHttpHandler handler = odata.createHandler(edm);
@@ -25,12 +24,8 @@ public class ProtonServlet extends HttpServlet {
 	    handler.register(new DefaultDebugSupport());
 	    log("about to handle request");
 	    handler.process(req, resp);
-	    log("handled request");
-	} catch (RuntimeException e) {
+	    log("handled request");}
+	catch (Exception e) {
 	    log(e.getMessage());
-	    throw new ServletException(e);
-	}
-	catch (SQLException | NamingException e) {
-	    throw new ServletException(e);}
-    }
-}
+	    throw new ServletException(e);}}}
+
