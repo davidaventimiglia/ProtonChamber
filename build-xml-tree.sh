91ed59d71c82c93e4796f8bad3f3bf152839606e
David A. Ventimiglia <dventimi@gmail.com>
2018-07-24 16:41:44 -0700

    Got PUT to work

--- build-xml-tree.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
- 
-echo "digraph G {"
-for LINK in $(xmlstarlet el -u $1 | grep -E -o '([^/]+)/([^/]+)$' | sed 's/\//->/g' | tr ':' '_')
-do
-     echo ${LINK}";"
-done
-echo "}"
--- src/main/java/org/protonchamber/ProtonEntityProcessor.java
+++ src/main/java/org/protonchamber/ProtonEntityProcessor.java
@@ -117,8 +117,28 @@ public class ProtonEntityProcessor implements EntityProcessor {
 	throw new UnsupportedOperationException();}
 
     @Override
-    public void updateEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo, ContentType requestFormat, ContentType responseFormat) {
-	throw new UnsupportedOperationException();}
+    public void updateEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo, ContentType requestFormat, ContentType responseFormat) throws ODataApplicationException, DeserializerException {
+	List<UriResource> parts = uriInfo.getUriResourceParts();
+	UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet)parts.get(0);
+	HttpMethod httpMethod = request.getMethod();
+	EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
+	EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+	InputStream requestInputStream = request.getBody();
+	ODataDeserializer deserializer = odata.createDeserializer(requestFormat);
+	DeserializerResult result = deserializer.entity(requestInputStream, edmEntityType);
+	Entity requestEntity = result.getEntity();
+	Map<String, String> pairs = new HashMap<>();
+	List<UriParameter> keyPredicates = uriResourceEntitySet.getKeyPredicates();
+	List<String> sqlPredicates = new ArrayList<>();
+	for (UriParameter p : keyPredicates) sqlPredicates.add(String.format("%s=%s", p.getName(), String.format("'%s'", p.getText())));
+	for (Property p : requestEntity.getProperties()) pairs.put(p.getName(), ""+p.getValue());
+	String update = String.format("update %s set %s where true and %s", edmEntitySet.getName(), pairs.toString().replace("}","").replace("{",""), String.join("and", sqlPredicates));
+	try (Statement s = conn.createStatement();
+	     AutoCloseableWrapper<Integer> rowCount = new AutoCloseableWrapper<>(s.executeUpdate(update))) {
+	    response.setStatusCode(HttpStatusCode.NO_CONTENT.getStatusCode());}
+	catch (Exception e) {
+	    servlet.log(e.getMessage(), e);
+	    throw new ODataApplicationException(String.format("message: %s, query: %s", e.getMessage(), update), 500, Locale.US);}}
 
     @Override
     public void readEntity (ODataRequest request,
