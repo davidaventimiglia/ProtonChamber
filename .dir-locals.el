7e1ab0215495d79f04dea3cc824f5d86ea050e19
David A. Ventimiglia <dventimi@gmail.com>
2018-10-28 11:06:59 -0700

    Tidying up a bit.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,22 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (compile-command . "cd `git rev-parse --show-toplevel` && mvn -B install"))
- (java-mode
-  (eval setq semanticdb-javap-classpath
-	(directory-files
-	 (concat
-	  (let
-	      ((l
-		(dir-locals-find-file
-		 (or
-		  (buffer-file-name)
-		  default-directory))))
-	    (if
-		(listp l)
-		(car l)
-	      l))
-	  "lib")
-	 t ".*.jar"))))
+  (compile-command . "cd `git rev-parse --show-toplevel` && mvn -B install")))
 
 
