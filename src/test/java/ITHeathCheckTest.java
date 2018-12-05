27b5653727bb996423b552dc71036168a6c302cb
David A. Ventimiglia <dventimi@gmail.com>
2018-12-04 20:25:28 -0800

    Using Docker for postgres in integration tests

--- src/test/java/ITHeathCheckTest.java
+++ src/test/java/ITHeathCheckTest.java
@@ -1,18 +1,25 @@
 package org.protonchamber;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
 import java.io.*;
 import java.net.*;
 import java.net.http.*;
+import java.sql.*;
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ITHealthCheckTest {
     @Test
-    void myFirstTest() {
+    void myFirstTest () {
         assertEquals(2, 1 + 1);}
 
+    @Test
+    void dbSantityCheck () {
+	try (Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:7432/atomic", "atomic", "atomic");
+	     Statement s = c.createStatement();
+	     ResultSet r = s.executeQuery("select now()")) {
+	    DatabaseMetaData m = c.getMetaData();}
+	catch (Exception e) {
+	    throw new RuntimeException(e);}}
 
     @Test
     public void givenUserDoesNotExists_whenUserInfoIsRetrieved_then404IsReceived () throws URISyntaxException, IOException, InterruptedException {
