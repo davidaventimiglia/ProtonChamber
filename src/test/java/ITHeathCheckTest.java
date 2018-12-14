edc59ca29d6e775c69e731eaf4c17101bca01413
David A. Ventimiglia <dventimi@gmail.com>
2018-12-14 07:37:42 -0800

    Trying to get integration tests to run

--- src/test/java/ITHeathCheckTest.java
+++ src/test/java/ITHeathCheckTest.java
@@ -5,18 +5,33 @@ import java.net.*;
 import java.net.http.*;
 import java.sql.*;
 import org.junit.jupiter.api.*;
+import org.stringtemplate.v4.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ITHealthCheckTest {
+    @BeforeAll
+    public void setUp () {
+    	ST st;
+    	STGroup g = new STGroupFile("default.stg");
+    	st = g.getInstanceOf("setUp");
+    	try (Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:7432/atomic", "atomic", "atomic");
+    	     Statement s = c.createStatement()) {
+    	    s.execute(st.render());}
+    	catch (Exception e) {
+    	    throw new RuntimeException(e);}}
+    
     @Test
     void myFirstTest () {
         assertEquals(2, 1 + 1);}
 
     @Test
     void dbSantityCheck () {
+	ST st;
+	STGroup g = new STGroupFile("default.stg");
+	st = g.getInstanceOf("test");
 	try (Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:7432/atomic", "atomic", "atomic");
 	     Statement s = c.createStatement();
-	     ResultSet r = s.executeQuery("select now()")) {
+	     ResultSet r = s.executeQuery(st.render())) {
 	    DatabaseMetaData m = c.getMetaData();}
 	catch (Exception e) {
 	    throw new RuntimeException(e);}}
