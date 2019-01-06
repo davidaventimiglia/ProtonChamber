6618a6bf17030fadeb9906540601a1c8ee60f794
David A. Ventimiglia <dventimi@gmail.com>
2019-01-06 08:24:29 -0800

    Got a rudimentary XML test to pass

--- src/test/java/ITHeathCheckTest.java
+++ src/test/java/ITHeathCheckTest.java
@@ -46,4 +46,23 @@ class ITHealthCheckTest {
     	    .proxy(ProxySelector.getDefault())
     	    .build()
     	    .send(request, HttpResponse.BodyHandlers.ofString());
-    	assertEquals(HttpURLConnection.HTTP_OK, response.statusCode());}}
+    	assertEquals(HttpURLConnection.HTTP_OK, response.statusCode());}
+
+    @Test
+    public void metadataDocumentCorrect () throws URISyntaxException, IOException, InterruptedException {
+        // try {this.transformer = TransformerFactory.newInstance().newTransformer(xslt);}
+        // catch (Throwable t) {throw new ServletException(t);}}
+        // try {transformer.transform(new StreamSource(getInputStream()), new StreamResult(target));}
+        // catch (Throwable t) {throw new IOException(t);}
+    	HttpRequest request = HttpRequest.newBuilder()
+    	    .uri(new URI("http://localhost:9080/ProtonChamber/ProtonService.svc/$metadata"))
+    	    .GET()
+    	    .build();
+    	HttpResponse<String> response = HttpClient.newBuilder()
+    	    .proxy(ProxySelector.getDefault())
+    	    .build()
+    	    .send(request, HttpResponse.BodyHandlers.ofString());
+	String targetXml = response.body();
+	String controlXml = "<?xml version='1.0' encoding='UTF-8'?><edmx:Edmx Version=\"4.0\" xmlns:edmx=\"http://docs.oasis-open.org/odata/ns/edmx\"><edmx:DataServices><Schema xmlns=\"http://docs.oasis-open.org/odata/ns/edm\" Namespace=\"public\"><EntityType Name=\"person\"><Key><PropertyRef Name=\"id\" Alias=\"id\"/></Key><Property Name=\"name\" Type=\"Edm.String\" Precision=\"2147483647\" Scale=\"0\"/><Property Name=\"id\" Type=\"Edm.Int32\" DefaultValue=\"nextval(&apos;person_id_seq&apos;::regclass)\" Precision=\"10\" Scale=\"0\"/></EntityType><EntityContainer Name=\"EntityContainer\"><EntitySet Name=\"person\" EntityType=\"public.person\"/></EntityContainer></Schema></edmx:DataServices></edmx:Edmx>";
+	assertEquals(controlXml, targetXml);}}
+    
